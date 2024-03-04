9032414c0096f27e63cdcd9a7c521894736ab582
Ruslan Shigapov <ilgamovich@gmail.com>
2024-03-04 13:11:54 +0300

    Added the project

---
+++ RubinScoutingApp/RubinScoutingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RubinScoutingApp
+//
+//  Created by Ruslan Shigapov on 04.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
